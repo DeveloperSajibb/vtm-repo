c86f18c80bec8fde6f55c307314bb66bd57e2b7d
Sajib_Ahmed <96062316+DeveloperSajibb@users.noreply.github.com>
2025-11-21 19:14:16 +0600

    Implement static file serving in router.php
    Add support for serving static files in PHP built-in server

--- public/router.php
+++ public/router.php
@@ -1,1 +1,11 @@
 
+<?php
+// Allow PHP built-in router to serve static files correctly
+if (php_sapi_name() === 'cli-server') {
+    $filePath = __DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    if (is_file($filePath)) {
+        return false;
+    }
+}
+
+require __DIR__ . '/index.php';
